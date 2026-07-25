161d5e9cdc437fff0c9d9a8c0fb95b9d59f60a01
toure-cloud <toure@cloud.com>
2026-07-25 20:27:26 +0000

    mise a jour

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -12,6 +12,9 @@ import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+// Ajouter ceci pour debug
+console.log('🔗 ADMIN_API_URL:', import.meta.env.VITE_ADMIN_API_URL)
+console.log('🔗 API_URL:', import.meta.env.VITE_API_URL)
 
 app.use(pinia)
 app.use(router)
